726574510bfaf1f35d0aadbdf60da6bd40db6afd
zanghong <718800822@qq.com>
2022-10-28 16:44:18 +0800

    [INTERNAL] first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	r.LoadHTMLFiles("./web/login.html")
+
+	r.Static("/sdk", "./web/sdk")
+	r.Static("/web", "./web")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", "")
+	})
+	r.POST("/login", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{})
+	})
+	r.Run(":8099")
+}
